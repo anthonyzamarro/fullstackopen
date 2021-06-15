a11db046c469b70af78406fb85dc5ffdc73f9fd0
Anthony <anthony.zamarro@gmail.com>
2021-06-15 06:22:19 -0400

    add parts objects to an array and combine all definitions into a variable

--- part1/exercises/app/src/App.js
+++ part1/exercises/app/src/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 const App = () => {
 
-	const course = 'Half Stack application development';
-	const part1 = {
-		name: 'Fundamentals of React',
-		exercises: 10
-	}
-	const part2 = {
-		name: 'Useing props to pass data',
-		exercises: 10
-	}
-	const part3 = {
-		name: 'State of a component',
-		exercises: 34
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+
+			{
+				name: 'Useing props to pass data',
+				exercises: 7
+			},
+
+			{
+				name: 'State of a component',
+				exercises: 34
+			}
+		]
 	}
 
 	const Header = (props) => (
@@ -24,28 +30,28 @@ const App = () => {
 
 	const Part = (props) => (
 		<>
-		<p>{props.content} {props.exercises}</p>
+			<p>{props.content} {props.exercises}</p>
 		</>
 	)
 
 	const Content = (props) => (
 		<>
-		<Part content={part1.name} exercises={part1.exercises}/>
-		<Part content={part2.name} exercises={part2.exercises}/>
-		<Part content={part3.name} exercises={part3.exercises}/>
+			<Part content={props.name} exercises={props.exercises}/>
 		</>
 	)
 
 	const Total = (props) => (
 		<>
-		<p>Number of exercises {props.total} </p>
+			<p>Number of exercises {props.total} </p>
 		</>
 	)
   return (
     <div>
-	    <Header title={course} />
-	    <Content />
-	    <Total total={part1.exercises + part2.exercises + part3.exercises } />
+	    <Header title={course.name} />
+	    {
+	    	course.parts.map(part => <Content name={part.name} exercises={part.exercises} />)
+	   }
+	    <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises } />
     </div>
   );
 }
