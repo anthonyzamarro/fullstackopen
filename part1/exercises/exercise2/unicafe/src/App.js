883a3ec33737fedb60f1b9edb7b2e896ec5bdb18
Anthony <anthony.zamarro@gmail.com>
2021-06-15 22:17:59 -0400

    add own state for each button click

--- part1/exercises/exercise2/unicafe/src/App.js
+++ part1/exercises/exercise2/unicafe/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClickGood = (val) => {
+    setGood(val + 1);
+  }
+  const handleClickNeutral = (val) => {
+    setNeutral(val + 1);
+  }
+  const handleClickBad = (val) => {
+    setBad(val + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => handleClickGood(good)}>good</button>
+      <button onClick={() => handleClickNeutral(neutral)}>neutral</button>
+      <button onClick={() => handleClickBad(bad)}>bad</button>
+      <br />
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </>
   );
 }
 
