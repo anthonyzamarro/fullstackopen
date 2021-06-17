d43f20391c7c121b30403eb6a280b20d7e1f6401
Anthony <anthony.zamarro@gmail.com>
2021-06-17 17:25:40 -0400

    extract stats display into own component

--- part1/exercises/exercise2/unicafe/src/App.js
+++ part1/exercises/exercise2/unicafe/src/App.js
@@ -1,34 +1,39 @@
 import React, {useState} from 'react';
 
+const Statistics = (props) => (
+  <div>
+    {props.type} {props.num}
+
+  </div>
+)
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
   const handleClickGood = (val, all) => {
     setGood(val + 1);
-    setAll(all + 1);
   }
   const handleClickNeutral = (val) => {
     setNeutral(val + 1);
-    setAll(all + 1);
   }
   const handleClickBad = (val) => {
     setBad(val + 1);
-    setAll(all + 1);
   }
+
+  const all = neutral + good + bad;
 
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => handleClickGood(good, all)}>good</button>
-      <button onClick={() => handleClickNeutral(neutral, all)}>neutral</button>
-      <button onClick={() => handleClickBad(bad, all)}>bad</button>
+      <button onClick={() => handleClickGood(good)}>good</button>
+      <button onClick={() => handleClickBad(bad)}>bad</button>
+      <button onClick={() => handleClickNeutral(neutral)}>neutral</button>
+      <Statistics  num={good} type="good"/>
+      <Statistics  num={bad} type="neutral"/>
+      <Statistics num={neutral} type="neutral"/>
       <br />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
       <div>all {all}</div>
       <div>average {all / 3}</div>
       <div>positive { `${(good / all) * 100}%` }</div>
