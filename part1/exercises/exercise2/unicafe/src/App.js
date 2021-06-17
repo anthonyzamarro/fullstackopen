3e99b50dd4d3c53f0c6890ecce05132d27e7d80d
Anthony <anthony.zamarro@gmail.com>
2021-06-17 17:15:45 -0400

    add all, average, and positive percentage to app

--- part1/exercises/exercise2/unicafe/src/App.js
+++ part1/exercises/exercise2/unicafe/src/App.js
@@ -4,27 +4,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
-  const handleClickGood = (val) => {
+  const handleClickGood = (val, all) => {
     setGood(val + 1);
+    setAll(all + 1);
   }
   const handleClickNeutral = (val) => {
     setNeutral(val + 1);
+    setAll(all + 1);
   }
   const handleClickBad = (val) => {
     setBad(val + 1);
+    setAll(all + 1);
   }
 
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => handleClickGood(good)}>good</button>
-      <button onClick={() => handleClickNeutral(neutral)}>neutral</button>
-      <button onClick={() => handleClickBad(bad)}>bad</button>
+      <button onClick={() => handleClickGood(good, all)}>good</button>
+      <button onClick={() => handleClickNeutral(neutral, all)}>neutral</button>
+      <button onClick={() => handleClickBad(bad, all)}>bad</button>
       <br />
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {all / 3}</div>
+      <div>positive { `${(good / all) * 100}%` }</div>
     </>
   );
 }
