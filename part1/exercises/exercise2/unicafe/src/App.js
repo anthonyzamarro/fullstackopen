e593dfc9e16ccbe227cf1d3a66b29fc5c7a4d872
Anthony <anthony.zamarro@gmail.com>
2021-06-18 23:28:45 -0400

    add random quote generator

--- part1/exercises/exercise2/unicafe/src/App.js
+++ part1/exercises/exercise2/unicafe/src/App.js
@@ -40,6 +40,24 @@ const App = () => {
   const all = neutral + good + bad;
   const positive = good / all;
 
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
+   
+  const [anecdote, setAnecdote] = useState(0);
+
+  const handleClickAnecdote = val => {
+    console.log(val)
+    setAnecdote(Math.floor(Math.random(val) * 10))
+  }
+
   return (
     <>
       <h1>give feedback</h1>
@@ -65,6 +83,9 @@ const App = () => {
             }
         </tbody>
     </table>
+      {anecdotes[anecdote] !== undefined ? anecdotes[anecdote] : anecdotes[0]}
+      <br/>
+      <Button onClick={() => handleClickAnecdote(anecdote)} type="anecdote" />
     </>
   );
 }
