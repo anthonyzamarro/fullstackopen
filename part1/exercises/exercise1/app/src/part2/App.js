f11c7385078f217ed5842bb89fb8c66c5900503e
Anthony <anthony.zamarro@gmail.com>
2021-06-30 19:10:38 -0400

    add click and onchange functions to form

--- part1/exercises/exercise1/app/src/part2/App.js
+++ part1/exercises/exercise1/app/src/part2/App.js
@@ -4,21 +4,40 @@ const App2 = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('');
+
+  const handleNameChange = e => {
+      setNewName(e.target.value);
+    }
+    
+    const handleClick = e => {
+      e.preventDefault();
+    
+      persons.forEach(person => {
+          if (person.name !== newName) {
+            setPersons(persons.concat({name: newName}));
+        } else {
+            return alert(`${newName} has already been added!`);
+          }
+      });
+
+      console.log(persons);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={persons.name} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {/* <div>debug: {newName}</div> */}
     </div>
   )
 }
